ab518c89d72438c4e217b5c8e6503122006017e4
Jorge A. Pérez <108704175+albertinho010@users.noreply.github.com>
2022-10-19 15:59:26 -0500

    Add files via upload

---
+++ cuadradro/main.js
@@ -0,0 +1,14 @@
+
+
+function cuadrado(lado) {
+    
+  lado = parseInt(prompt("Ingreso el lado"));
+  console.log('El lado es ' + lado + ' unidades')
+  let area; 
+  area = (lado*lado);
+  alert('Si el lado mide ' + lado + ' unidades, entonces el área es de ' + area + ' unidades cuadradas');
+  console.log('Si el lado mide ' + lado + ' unidades, entonces el área es de ' + area + ' unidades cuadradas')
+}
+  console.log(cuadrado(0,0));
+
+  
