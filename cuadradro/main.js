e27974b9015f732766d8638ec0789470d6438654
Jorge A. Pérez <108704175+albertinho010@users.noreply.github.com>
2022-10-20 17:37:48 -0500

    terminando la función

--- cuadradro/main.js
+++ cuadradro/main.js
@@ -1,14 +1,14 @@
 
-
+lado = parseInt(prompt("Ingreso el lado"));
 function cuadrado(lado) {
     
-  lado = parseInt(prompt("Ingreso el lado"));
+  
   console.log('El lado es ' + lado + ' unidades')
   let area; 
   area = (lado*lado);
   alert('Si el lado mide ' + lado + ' unidades, entonces el área es de ' + area + ' unidades cuadradas');
   console.log('Si el lado mide ' + lado + ' unidades, entonces el área es de ' + area + ' unidades cuadradas')
 }
-  console.log(cuadrado(0,0));
+  cuadrado(lado);
 
   
