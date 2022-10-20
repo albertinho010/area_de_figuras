1e83b37f71f612f6945bc8f81e6f73d760756e5c
Jorge A. Pérez <108704175+albertinho010@users.noreply.github.com>
2022-10-20 17:40:28 -0500

    sacando los dialogos

--- rectangulo/main.js
+++ rectangulo/main.js
@@ -1,17 +1,16 @@
 
-
-
-
-  
-function rectangulo(largo, ancho) {
-  largo = parseInt(prompt("Ingreso el largo del rectángulo"));
+largo = parseInt(prompt("Ingreso el largo del rectángulo"));
   console.log('El largo es ' + largo + ' unidades')
   ancho = parseInt(prompt("Ingreso el ancho del rectángulo"));
   console.log('El ancho es ' + ancho + ' unidades')
+
+  
+function rectangulo(largo, ancho) {
+  
   let area;
   area = (largo*ancho);
   alert('Si el largo mide ' + largo + ' unidades y el ancho mide' + ancho + ' unidades, entonces el área es de ' + area + ' unidades cuadradas');
   console.log('Si el largo mide ' + largo + ' unidades y el ancho mide' + ancho + ' unidades, entonces el área es de ' + area + ' unidades cuadradas')
 }
-console.log(rectangulo(0,0));
+rectangulo(largo, ancho);
 
